3c131ee2ba2a1a0bdcb4362ee0efb0a75d7e9c69
Nico <jnch2005@gmail.com>
2023-09-06 15:06:15 -0500

    pruebas y metodo listo ;)))))

--- preparcial/src/main/java/com/MaquinaExpendedora.java
+++ preparcial/src/main/java/com/MaquinaExpendedora.java
@@ -5,9 +5,27 @@ public class MaquinaExpendedora {
     private int valorPagado;
 
     public MaquinaExpendedora(int valorPagado, int valorPagar){
-        
+        assert valorPagado >= 0;
+        assert valorPagar >= 0;
+        assert valorPagado > valorPagar;
         this.valorPagado = valorPagado;
         this.valorPagar = valorPagar;
+    }
+
+    public int getValorPagar() {
+        return valorPagar;
+    }
+
+    public void setValorPagar(int valorPagar) {
+        this.valorPagar = valorPagar;
+    }
+
+    public int getValorPagado() {
+        return valorPagado;
+    }
+
+    public void setValorPagado(int valorPagado) {
+        this.valorPagado = valorPagado;
     }
     
 }
--- preparcial/src/test/java/AppTest.java
+++ preparcial/src/test/java/AppTest.java
@@ -5,14 +5,13 @@
  * 
  * Licencia GNU/GPL V3.0 (https://raw.githubusercontent.com/grid-uq/poo/main/LICENSE) 
  */
-
-import java.time.LocalDate;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
+
+import com.MaquinaExpendedora;
 
 /**
  * Unit test for simple App.
@@ -26,7 +25,24 @@ public class AppTest {
     @Test
     public void datosCorrectos() {
         LOG.info("Iniciado test Datos correctos.....");
-        
+        MaquinaExpendedora maquinaExpendedora = new MaquinaExpendedora(1500, 1000);
+        assertEquals(1000, maquinaExpendedora.getValorPagar());
+        assertEquals(1500, maquinaExpendedora.getValorPagado());
         LOG.info("Finalizando test Datos correctos.....");
+        
+    }
+    @Test 
+    public void datosNegativos(){
+        LOG.info("Iniciado test Datos negativos.....");
+        assertThrows(Throwable.class, ()-> new MaquinaExpendedora(-1500, -10000));
+        LOG.info("Finalizando test Datos negativos.....");
+
+    }
+    @Test 
+    public void datosPagarSuperior(){
+        LOG.info("Iniciado test Datos pagar superior.....");
+        assertThrows(Throwable.class, ()-> new MaquinaExpendedora(1000, 1500));
+        LOG.info("Finalizando test Datos pagar superior.....");
+
     }
 }
