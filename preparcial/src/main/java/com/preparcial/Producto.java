9c709bd179921e80b87bebc45e5acddc8892dc22
Nico <jnch2005@gmail.com>
2023-09-06 15:36:10 -0500

    finalizados metodos constructores

--- preparcial/src/main/java/com/preparcial/Producto.java
+++ preparcial/src/main/java/com/preparcial/Producto.java
@@ -5,11 +5,15 @@ public class Producto {
     private String name;
     private LocalDate fechaVencimiento;
     private int precio;
-    
+    public Marca marca;
+    public TipoProducto tipoProducto;
+
     public Producto(String name, LocalDate fechaVencimiento, int precio, Marca marca, TipoProducto tipoProducto){
         this.name = name;
         this.fechaVencimiento = fechaVencimiento;
         this.precio = precio;
+        this.marca = marca;
+        this.tipoProducto = tipoProducto;
     }
 
     public String getName() {
@@ -22,6 +26,14 @@ public class Producto {
 
     public int getPrecio() {
         return precio;
+    }
+    
+    public Marca getMarca(){
+        return marca;
+    }
+
+    public TipoProducto getTipoProducto() {
+        return tipoProducto;
     }
     
 }
